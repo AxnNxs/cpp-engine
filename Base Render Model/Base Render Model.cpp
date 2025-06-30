f93390549bc405e8a2c21bc0c487674b0c737a44
Matt <axisray@live.it>
2025-06-30 16:38:23 +0200

    Add project files.

---
+++ Base Render Model/Base Render Model.cpp
@@ -0,0 +1,12 @@
+﻿// Base Render Model.cpp : Defines the entry point for the application.
+//
+
+#include "Base Render Model.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
